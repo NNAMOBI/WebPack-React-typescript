661d6ad82f3de9cc5cc9bcb8b8dac2d2b4fa0af4
NNAMOBI <nnamosuagwu84@gmail.com>
2024-02-16 02:24:48 +0000

    created two separate webpack config files for dev and for prod. then a common.js file. Also added a react refresher to maintain state of the app when other parts are updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import ClickCounter from './ClickCounter';
 import './styles.css';
 
 const App = () => {
     return (
-        <h1>Welcome to Webpack starter react Configuration</h1>
-    )
+      <>
+        <h1>
+          Hello Everyone!! Welcome to Webpack starter react Configuration -{" "}
+          {process.env.NODE_ENV}- {process.env.name}
+            </h1>
+            <ClickCounter />
+      </>
+    );
 }
 
 export default App;
